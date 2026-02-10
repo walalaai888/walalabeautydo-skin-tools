51c7c1e56547de479cc4f96f463ee24f7729ae56
walalaai888 <walalaai888@gmail.com>
2026-02-11 01:11:55 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useMemo, useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
